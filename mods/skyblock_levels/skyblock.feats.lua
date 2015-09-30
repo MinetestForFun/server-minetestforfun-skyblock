4bb0869ada32fb8b94a039a97d913da2074673d9
LeMagnesium <mg.minetest@gmail.com>
2015-09-30 16:20:27 +0200

    [skyblock_levels/feats] Add dead code for future sounds  - Related to #4

--- mods/skyblock_levels/skyblock.feats.lua
+++ mods/skyblock_levels/skyblock.feats.lua
@@ -66,6 +66,10 @@ function skyblock.feats.update(player_name)
 		--minetest.chat_send_player(player_name, 'You completed level '..level)
 		minetest.chat_send_all(player_name..' completed level '..level)
 		irc:say(player_name .. ' completed level ' .. level)
+--		minetest.sound_play("skyblock_finish_level_player", {
+--			to_player = player_name,
+--			gain = 1.0
+--		})
 		minetest.log('action', player_name..' completed level '..level)
 		
 		skyblock.feats.add(0,info.player_name,'level')
@@ -111,6 +115,14 @@ function skyblock.feats.add(level,player_name,feat)
 		if rewarded then
 			minetest.chat_send_all(player_name..' completed the quest "'..feat..'" on level '..level)
 			irc:say(player_name .. ' completed the quest "' .. feat .. '" on level ' .. level)
+--			for _, ref in pairs({minetest.get_connected_players()}) do
+--				if not ref:get_player_name() == player_name then
+--					minetest.sound_play("skyblock_finish_other", {
+--						to_player = ref:get_player_name(),
+--						gain = 1.0,
+--					})
+--				end
+--			end
 			minetest.log('action', player_name..' completed the quest "'..feat..'" on level '..level)
 		end
 	end
@@ -127,6 +139,10 @@ function skyblock.feats.set(level,player_name,feat,value)
 		if rewarded then
 			minetest.chat_send_all(player_name..' completed the quest "'..feat..'" on level '..level)
 			irc:say(player_name..' completed the quest "'..feat..'" on level '..level)
+--			minetest.sound_play("skyblock_finish_quest", {
+--				to_player = player_name,
+--				gain = 1.0,
+--			})
 			minetest.log('action', player_name..' completed the quest "'..feat..'" on level '..level)
 		end
 	end
