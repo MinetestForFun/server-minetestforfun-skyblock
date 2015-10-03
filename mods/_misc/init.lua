c7a8ee35a3c4dec57118d3ef762e4faf98bcbc9c
LeMagnesium <mg.minetest@gmail.com>
2015-10-03 12:45:12 +0200

    [_misc] Improve privs granter to not remove previous privileges

--- mods/_misc/init.lua
+++ mods/_misc/init.lua
@@ -6,5 +6,11 @@
 -- Give set privileges
 minetest.register_on_joinplayer(function(player)
 	local p = minetest.setting_get("default_privs")
-	minetest.set_player_privs(player:get_player_name(), minetest.string_to_privs(p))
+	local u = minetest.get_player_privs(player:get_player_name())
+	local n = minetest.string_to_privs(p)
+
+	for priv, _ in pairs(u) do
+		n[priv] = true
+	end
+	minetest.set_player_privs(player:get_player_name(), n)
 end)
