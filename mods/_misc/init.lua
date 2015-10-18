35ab1e9d41c27b10fddd93a0ada017fc98a58b11
LeMagnesium <mg.minetest@gmail.com>
2015-10-18 22:45:39 +0200

    [_misc & conf] Put back fly and fast as default privs

--- mods/_misc/init.lua
+++ mods/_misc/init.lua
@@ -9,14 +9,15 @@ minetest.register_on_joinplayer(function(player)
 	local u = minetest.get_player_privs(player:get_player_name())
 	local n = minetest.string_to_privs(p)
 
+	for priv, _ in pairs(n) do
+		u[priv] = true
+	end
+
 	if skyblock.feats.get_level(player:get_player_name()) < 4 and (u['fly'] or u['fast']) then
 		u['fly'] = nil
 		u['fast'] = nil
 		minetest.chat_send_player(player:get_player_name(), "You have lost FLY and FAST")
 	end
 
-	for priv, _ in pairs(n) do
-		u[priv] = true
-	end
 	minetest.set_player_privs(player:get_player_name(), n)
 end)
