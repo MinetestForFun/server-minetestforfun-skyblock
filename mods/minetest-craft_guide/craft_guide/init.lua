bd4fc522c7386541834171a7d59533635debcefe
LeMagnesium <mg.minetest@gmail.com>
2015-10-28 19:53:37 +0100

    [craft_guide] Retrograde stored tables for the craft_guide

--- mods/minetest-craft_guide/craft_guide/init.lua
+++ mods/minetest-craft_guide/craft_guide/init.lua
@@ -27,19 +27,36 @@ minetest.register_alias = function (name, convert_to)
 	craft_guide.register_alias(name, convert_to)
 end
 
+local function retrograde_table(recipe, w)
+	local tab = {}
+	for i = 1, 3 do
+		tab[i] = {}
+	end
+	for i, v in pairs(recipe) do
+		i = i - 1
+		local j = i%w
+		local k = (i-j) / w
+		tab[k+1][j+1] = v
+	end
+	return tab
+end
+
 minetest.after(1, function()
 	for item, def in pairs(minetest.registered_items) do
 		if minetest.get_item_group(item, "not_in_creative_inventory") == 0 and def.description ~= "" then
-			crafts = minetest.get_all_craft_recipes(item)
+			local crafts = minetest.get_all_craft_recipes(item)
 			if crafts then
 				for _, tab in pairs(crafts) do
-					if tab.type == "normal" then
+					if tab.width == 0 then
+						tab.type = "shapeless"
+					elseif tab.type == "normal" then
 						tab.type = nil
+						tab.items = retrograde_table(tab.items, tab.width)
 					end
 					craft_guide.register_craft({
 						type = tab.type,
 						output = tab.output,
-						recipe = {tab.items},
+						recipe = tab.items,
 						replacements = tab["replacements"]
 					})
 				end
