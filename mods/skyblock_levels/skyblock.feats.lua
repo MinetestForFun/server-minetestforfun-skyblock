de5a071acc760bc8e7e54edb07c8fb45af12d802
LeMagnesium <mg.minetest@gmail.com>
2015-10-29 15:10:03 +0100

    [skyblock/feats] Play finish sounds in both setfeat and addfeat

--- mods/skyblock_levels/skyblock.feats.lua
+++ mods/skyblock_levels/skyblock.feats.lua
@@ -121,6 +121,11 @@ function skyblock.feats.add(level,player_name,feat)
 						to_player = ref:get_player_name(),
 						gain = 1.0,
 					})
+				else
+					minetest.sound_play("skyblock_finish_quest", {
+						to_player = player_name,
+						gain = 1.0,
+					})
 				end
 			end
 			minetest.log('action', player_name..' completed the quest "'..feat..'" on level '..level)
@@ -139,10 +144,19 @@ function skyblock.feats.set(level,player_name,feat,value)
 		if rewarded then
 			minetest.chat_send_all(player_name..' completed the quest "'..feat..'" on level '..level)
 			irc:say(player_name..' completed the quest "'..feat..'" on level '..level)
-			minetest.sound_play("skyblock_finish_quest", {
-				to_player = player_name,
-				gain = 1.0,
-			})
+			for _, ref in pairs(minetest.get_connected_players()) do
+				if not (ref:get_player_name() == player_name) then
+					minetest.sound_play("skyblock_finish_other", {
+						to_player = ref:get_player_name(),
+						gain = 1.0,
+					})
+				else
+					minetest.sound_play("skyblock_finish_quest", {
+						to_player = player_name,
+						gain = 1.0,
+					})
+				end
+			end
 			minetest.log('action', player_name..' completed the quest "'..feat..'" on level '..level)
 		end
 	end
