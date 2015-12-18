45c7740dfd6aef37753c4d905d4cf181972808c6
LeMagnesium <mg.minetest@gmail.com>
2015-12-18 21:01:57 +0100

    [sethome] Create global sethome.set_home and sethome.go_home
    - And also sethome.get_home
    - Needed for #44

--- mods/sethome/init.lua
+++ mods/sethome/init.lua
@@ -1,8 +1,9 @@
-local homes_file = minetest.get_worldpath() .. "/homes"
+sethome = {}
+sethome.homes_file = minetest.get_worldpath() .. "/homes"
 local homepos = {}
 
 local function loadhomes()
-    local input = io.open(homes_file, "r")
+    local input = io.open(sethome.homes_file, "r")
     if input then
 		repeat
             local x = input:read("*n")
@@ -26,40 +27,50 @@ minetest.register_privilege("home", "Can use /sethome and /home")
 
 local changed = false
 
+function sethome.go_home(name)
+	local player = minetest.get_player_by_name(name)
+	if player == nil then
+		-- just a check to prevent the server crashing
+		return false
+	end
+
+	if homepos[player:get_player_name()] then
+		player:setpos(homepos[player:get_player_name()])
+		minetest.chat_send_player(name, "Teleported to home!")
+	else
+		minetest.chat_send_player(name, "Set a home using /sethome")
+	end
+	return true
+end
+
 minetest.register_chatcommand("home", {
     description = "Teleport you to your home point",
     privs = {home=true},
-    func = function(name)
-        local player = minetest.get_player_by_name(name)
-        if player == nil then
-            -- just a check to prevent the server crashing
-            return false
-        end
-        if homepos[player:get_player_name()] then
-            player:setpos(homepos[player:get_player_name()])
-            minetest.chat_send_player(name, "Teleported to home!")
-        else
-            minetest.chat_send_player(name, "Set a home using /sethome")
-        end
-    end,
+    func = sethome.go_home
 })
+
+function sethome.set_home(name)
+	local player = minetest.get_player_by_name(name)
+	local pos = player:getpos()
+	homepos[player:get_player_name()] = pos
+	minetest.chat_send_player(name, "Home set!")
+	changed = true
+	if changed then
+		local output = io.open(sethome.homes_file, "w")
+		for i, v in pairs(homepos) do
+			output:write(v.x.." "..v.y.." "..v.z.." "..i.."\n")
+		end
+		io.close(output)
+		changed = false
+	end
+end
 
 minetest.register_chatcommand("sethome", {
     description = "Set your home point",
     privs = {home=true},
-    func = function(name)
-        local player = minetest.get_player_by_name(name)
-        local pos = player:getpos()
-        homepos[player:get_player_name()] = pos
-        minetest.chat_send_player(name, "Home set!")
-        changed = true
-        if changed then
-        	local output = io.open(homes_file, "w")
-            for i, v in pairs(homepos) do
-                output:write(v.x.." "..v.y.." "..v.z.." "..i.."\n")
-            end
-            io.close(output)
-            changed = false
-        end
-    end,
+    func = sethome.set_home
 })
+
+function sethome.get_home(name)
+	return homepos[name]
+end
