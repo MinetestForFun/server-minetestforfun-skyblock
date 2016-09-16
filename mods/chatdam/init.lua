350909620328c66c768f746f73b99a139db0213a
LeMagnesium <mg.minetest@gmail.com>
2016-09-16 18:21:56 +0200

    [chatdam] New implementation; for #170
     - Kick player sending a message that is longer than 255 characters (by default)
     - Kick player sending too many messages in a defined interval (here,
       more than 3 messages in 2 seconds by default)
     - If any violation, the messages are discarded and never sent

--- mods/chatdam/init.lua
+++ mods/chatdam/init.lua
@@ -10,61 +10,51 @@
 
 chatdam = {}
 
+chatdam.data = {}
+
+-- Retrieve some config stuff
 chatdam.conf = {}
-chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE = tonumber(minetest.setting_get("max_chars_per_msg") or 255)
-chatdam.conf.TRUNCATE_LONG_MESSAGES = minetest.setting_get("truncate_long_msg")
-chatdam.conf.MESSAGE_DELAY = tonumber(minetest.setting_get("message_delay") or 1.5)
+chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE = tonumber(minetest.setting_get("maximum_characters_per_message") or "255")
+chatdam.conf.MAXIMUM_MESSAGES_PER_INTERVAL = tonumber(minetest.setting_get("maximum_messages_per_interval") or "3")
+chatdam.conf.FLOOD_INTERVAL = tonumber(minetest.setting_get("flood_interval") or "2")
 
---if(table.getn(core.registered_on_chat_messages) > 1) then
---	error("CHATDAM MUST BE LOADED WHEN NO OTHER CALLBACK OTHER THAN THE ONE TO BE WIPED EXIST. PLEASE FIND THE MOD LOADED BEFORE CHATDAM WHICH REGISTERS A CALLBACK FOR CHAT MESSAGES AND ADD 'chatdam' TO ITS depends.txt FILE")
---end
+-- Main control function. A real nightmare for flooders.
+function chatdam.floodcontrol(name, message)
+	local violation
 
+	-- Kick for message length
+	if message:len() > chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE then
+		violation = ("Your sent a message with more characters than allowed (%s)"):format(chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE)
+	end
 
-chatdam.emulate_message = function(name, message)
-	-- Normal log handling
-	minetest.log(("CHAT: <%s> %s"):format(name, message))
-	-- First call other callbacks
-	for _, cback in pairs(core.registered_on_chat_messages) do
-		if (cback ~= chatdam.floodcontrol) then
-			if (cback(name, message)) then
-				return true
-			end
+	-- Kick for message frequency
+	local rightnow = tonumber(os.date("%s"))
+	-- By the power of maths I check the last <interval> seconds
+	if rightnow > chatdam.data[name].lastmsg+chatdam.conf.FLOOD_INTERVAL then
+		chatdam.data[name].msgs = 1
+		chatdam.data[name].lastmsg = rightnow
+	else
+		chatdam.data[name].msgs = chatdam.data[name].msgs + 1
+		if chatdam.data[name].msgs > chatdam.conf.MAXIMUM_MESSAGES_PER_INTERVAL then
+			violation = ("You sent too many messages in a short amount of time (maximum allowed is %s for %s seconds)"):format(chatdam.conf.MAXIMUM_MESSAGES_PER_INTERVAL, chatdam.conf.FLOOD_INTERVAL)
 		end
 	end
 
-	-- Then send it
-	for _, pref in pairs(minetest.get_connected_players()) do
-		if (pref:get_player_name() ~= name) then
-			minetest.chat_send_player(pref:get_player_name(), ("<%s> %s"):format(name, message))
-		end
+	if violation then
+		minetest.kick_player(name, violation)
+		return true -- Cancel anything that might happen after, and the engine's response, which is to send the message
 	end
-	-- Boom. Done
-end
-
-
-chatdam.floodcontrol = function(name, message)
-	local strlen = string.len(message)
-	local delayadd = 0
-	while strlen > chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE do
-		local nstr = string.sub(message, 0, chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE)
-		minetest.after(delayadd * chatdam.conf.MESSAGE_DELAY, chatdam.emulate_message, name, nstr)
-		if chatdam.conf.TRUNCATE_LONG_MESSAGES then
-			-- We're done, f*ck the rest of the message
-			minetest.chat_send_player(name, "-!- Your message was truncated")
-			return true
-		end
-		message = string.sub(message, chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE+1)
-		strlen = strlen-chatdam.conf.MAXIMUM_CHARACTERS_PER_MESSAGE
-		delayadd = delayadd + 1
-	end
-
-	if (strlen > 0) then
-		minetest.after(delayadd * chatdam.conf.MESSAGE_DELAY, chatdam.emulate_message, name, message)
-	end
-
-	return true -- Cancel anything that might happen after, and the engine's response
 end
 minetest.register_on_chat_message(chatdam.floodcontrol)
 
+-- We create tracking data for the players' messages here...
+minetest.register_on_joinplayer(function(pref)
+	chatdam.data[pref:get_player_name()] = {lastmsg=0, msgs=0}
+end)
+
+-- ... and we discard it here
+minetest.register_on_leaveplayer(function(pref)
+	chatdam.data[pref:get_player_name()] = nil
+end)
 
 minetest.log("[ChatDam] Loaded")
