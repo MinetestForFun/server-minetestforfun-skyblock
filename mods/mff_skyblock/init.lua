d652fb8c1fc5b784c825fb13a278c053f5e36a45
crabman77 <crabman77.github@gmail.com>
2016-09-30 18:11:42 +0200

    add toggle privs to cessna151 and update protector submodule, fix issue https://github.com/MinetestForFun/server-minetestforfun-skyblock/issues/186

--- mods/mff_skyblock/init.lua
+++ mods/mff_skyblock/init.lua
@@ -52,7 +52,7 @@ skyblock.feats.privilege_grid = {
 }
 
 local privileged = {
-	["cessna151"] = {"kick", "protection_bypass", "delprotect", "ignore_protection", "teleport", "ban"},
+	["cessna151"] = {"kick", "toggle", "protection_bypass", "delprotect", "ignore_protection", "teleport", "ban"},
 	["Mg"] = {"kick", "ban", "irc_admin"},
 	["crabman"] = {"kick", "ban", "irc_admin"},
 }
