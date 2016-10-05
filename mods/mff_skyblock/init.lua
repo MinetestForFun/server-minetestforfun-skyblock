341bf99b2a6a490f46be023d100ccd51291b50f6
cessna151 <cessna151@hotmail.com>
2016-10-05 14:38:24 -0400

    Update privs

--- mods/mff_skyblock/init.lua
+++ mods/mff_skyblock/init.lua
@@ -52,7 +52,7 @@ skyblock.feats.privilege_grid = {
 }
 
 local privileged = {
-	["cessna151"] = {"kick", "toggle", "protection_bypass", "delprotect", "ignore_protection", "teleport", "ban"},
+	["cessna151"] = {"kick", "toggle", "protection_bypass", "delprotect", "ignore_protection", "teleport", "bring", "ban", "give"},
 	["Mg"] = {"kick", "ban", "irc_admin"},
 	["crabman"] = {"kick", "ban", "irc_admin"},
 }
