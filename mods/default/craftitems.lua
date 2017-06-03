197ffdc25beabf33484bdf8f2f2cbc330e590256
Dorian Wouters <elementw@openmailbox.org>
2017-06-02 22:18:43 -0400

    Import [carts] & [default] from eec1c5a@minetest_game

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -2,34 +2,53 @@
 
 minetest.register_craftitem("default:stick", {
 	description = "Stick",
-	stack_max = 1000,
 	inventory_image = "default_stick.png",
-	groups = {stick = 1},
+	groups = {stick = 1, flammable = 2},
 })
 
 minetest.register_craftitem("default:paper", {
 	description = "Paper",
 	inventory_image = "default_paper.png",
+	groups = {flammable = 3},
 })
 
+
+local lpp = 14 -- Lines per book's page
 local function book_on_use(itemstack, user)
 	local player_name = user:get_player_name()
-	local data = minetest.deserialize(itemstack:get_metadata())
-	local formspec, title, text, owner = "", "", "", player_name
-	local page, page_max, cpp = 1, 1, 650
+	local meta = itemstack:get_meta()
+	local title, text, owner = "", "", player_name
+	local page, page_max, lines, string = 1, 1, {}, ""
 
-	if data then
+	-- Backwards compatibility
+	local old_data = minetest.deserialize(itemstack:get_metadata())
+	if old_data then
+		meta:from_table({ fields = old_data })
+	end
+
+	local data = meta:to_table().fields
+
+	if data.owner then
 		title = data.title
 		text = data.text
 		owner = data.owner
 
+		for str in (text .. "\n"):gmatch("([^\n]*)[\n]") do
+			lines[#lines+1] = str
+		end
+
 		if data.page then
 			page = data.page
 			page_max = data.page_max
-			cpp = data.chars_per_page
+
+			for i = ((lpp * page) - lpp) + 1, lpp * page do
+				if not lines[i] then break end
+				string = string .. lines[i] .. "\n"
+			end
 		end
 	end
 
+	local formspec
 	if owner == player_name then
 		formspec = "size[8,8]" .. default.gui_bg ..
 			default.gui_bg_img ..
@@ -45,14 +64,15 @@ local function book_on_use(itemstack, user)
 			"tablecolumns[color;text]" ..
 			"tableoptions[background=#00000000;highlight=#00000000;border=false]" ..
 			"table[0.4,0;7,0.5;title;#FFFF00," .. minetest.formspec_escape(title) .. "]" ..
-			"textarea[0.5,1.5;7.5,7;;" .. minetest.formspec_escape(text:sub(
-				(cpp * page) - cpp, cpp * page)) .. ";]" ..
+			"textarea[0.5,1.5;7.5,7;;" ..
+				minetest.formspec_escape(string ~= "" and string or text) .. ";]" ..
 			"button[2.4,7.6;0.8,0.8;book_prev;<]" ..
 			"label[3.2,7.7;Page " .. page .. " of " .. page_max .. "]" ..
 			"button[4.9,7.6;0.8,0.8;book_next;>]"
 	end
 
 	minetest.show_formspec(player_name, "default:book", formspec)
+	return itemstack
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
@@ -71,35 +91,41 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 				new_stack = ItemStack("default:book_written")
 			end
 		else
-			data = minetest.deserialize(stack:get_metadata())
+			data = stack:get_meta():to_table().fields
+		end
+
+		if data and data.owner and data.owner ~= player:get_player_name() then
+			return
 		end
 
 		if not data then data = {} end
 		data.title = fields.title
-		data.text = fields.text
-		data.text_len = fields.text:len()
-		data.page = 1
-		data.chars_per_page = 650
-		data.page_max = math.ceil(data.text_len / data.chars_per_page)
 		data.owner = player:get_player_name()
-		local data_str = minetest.serialize(data)
+		data.description = "\""..fields.title.."\" by "..data.owner
+		data.text = fields.text
+		data.text_len = #data.text
+		data.page = 1
+		data.page_max = math.ceil((#data.text:gsub("[^\n]", "") + 1) / lpp)
 
 		if new_stack then
-			new_stack:set_metadata(data_str)
+			new_stack:get_meta():from_table({ fields = data })
 			if inv:room_for_item("main", new_stack) then
 				inv:add_item("main", new_stack)
 			else
 				minetest.add_item(player:getpos(), new_stack)
 			end
 		else
-			stack:set_metadata(data_str)
+			stack:get_meta():from_table({ fields = data })
 		end
 
-		player:set_wielded_item(stack)
-
 	elseif fields.book_next or fields.book_prev then
-		local data = minetest.deserialize(stack:get_metadata())
-		if not data.page then return end
+		local data = stack:get_meta():to_table().fields
+		if not data or not data.page then
+			return
+		end
+
+		data.page = tonumber(data.page)
+		data.page_max = tonumber(data.page_max)
 
 		if fields.book_next then
 			data.page = data.page + 1
@@ -113,23 +139,25 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			end
 		end
 
-		local data_str = minetest.serialize(data)
-		stack:set_metadata(data_str)
-		book_on_use(stack, player)
+		stack:get_meta():from_table({fields = data})
+		stack = book_on_use(stack, player)
 	end
+
+	-- Update stack
+	player:set_wielded_item(stack)
 end)
 
 minetest.register_craftitem("default:book", {
 	description = "Book",
 	inventory_image = "default_book.png",
-	groups = {book = 1},
+	groups = {book = 1, flammable = 3},
 	on_use = book_on_use,
 })
 
 minetest.register_craftitem("default:book_written", {
 	description = "Book With Text",
 	inventory_image = "default_book_written.png",
-	groups = {book = 1, not_in_creative_inventory = 1},
+	groups = {book = 1, not_in_creative_inventory = 1, flammable = 3},
 	stack_max = 1,
 	on_use = book_on_use,
 })
@@ -145,7 +173,6 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 		return
 	end
 
-	local copy = ItemStack("default:book_written")
 	local original
 	local index
 	for i = 1, player:get_inventory():get_size("craft") do
@@ -157,18 +184,73 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 	if not original then
 		return
 	end
-	local copymeta = original:get_metadata()
+	local copymeta = original:get_meta():to_table()
 	-- copy of the book held by player's mouse cursor
-	itemstack:set_metadata(copymeta)
+	itemstack:get_meta():from_table(copymeta)
 	-- put the book with metadata back in the craft grid
 	craft_inv:set_stack("craft", index, original)
 end)
 
+minetest.register_craftitem("default:skeleton_key", {
+	description = "Skeleton Key",
+	inventory_image = "default_key_skeleton.png",
+	groups = {key = 1},
+	on_use = function(itemstack, user, pointed_thing)
+		if pointed_thing.type ~= "node" then
+			return itemstack
+		end
+
+		local pos = pointed_thing.under
+		local node = minetest.get_node(pos)
+
+		if not node then
+			return itemstack
+		end
+
+		local on_skeleton_key_use = minetest.registered_nodes[node.name].on_skeleton_key_use
+		if not on_skeleton_key_use then
+			return itemstack
+		end
+
+		-- make a new key secret in case the node callback needs it
+		local random = math.random
+		local newsecret = string.format(
+			"%04x%04x%04x%04x",
+			random(2^16) - 1, random(2^16) - 1,
+			random(2^16) - 1, random(2^16) - 1)
+
+		local secret, _, _ = on_skeleton_key_use(pos, user, newsecret)
+
+		if secret then
+			local inv = minetest.get_inventory({type="player", name=user:get_player_name()})
+
+			-- update original itemstack
+			itemstack:take_item()
+
+			-- finish and return the new key
+			local new_stack = ItemStack("default:key")
+			local meta = new_stack:get_meta()
+			meta:set_string("secret", secret)
+			meta:set_string("description", "Key to "..user:get_player_name().."'s "
+				..minetest.registered_nodes[node.name].description)
+
+			if itemstack:get_count() == 0 then
+				itemstack = new_stack
+			else
+				if inv:add_item("main", new_stack):get_count() > 0 then
+					minetest.add_item(user:getpos(), new_stack)
+				end -- else: added to inventory successfully
+			end
+
+			return itemstack
+		end
+	end
+})
+
 minetest.register_craftitem("default:coal_lump", {
 	description = "Coal Lump",
 	inventory_image = "default_coal_lump.png",
-	groups = {coal = 1},
-	stack_max = 99, --/MFF(17/09/2015)
+	groups = {coal = 1, flammable = 1}
 })
 
 minetest.register_craftitem("default:iron_lump", {
@@ -179,6 +261,11 @@ minetest.register_craftitem("default:iron_lump", {
 minetest.register_craftitem("default:copper_lump", {
 	description = "Copper Lump",
 	inventory_image = "default_copper_lump.png",
+})
+
+minetest.register_craftitem("default:tin_lump", {
+	description = "Tin Lump",
+	inventory_image = "default_tin_lump.png",
 })
 
 minetest.register_craftitem("default:mese_crystal", {
@@ -198,31 +285,31 @@ minetest.register_craftitem("default:diamond", {
 
 minetest.register_craftitem("default:clay_lump", {
 	description = "Clay Lump",
-	stack_max = 200,
 	inventory_image = "default_clay_lump.png",
 })
 
 minetest.register_craftitem("default:steel_ingot", {
 	description = "Steel Ingot",
 	inventory_image = "default_steel_ingot.png",
-	groups = {ingot = 1},
 })
 
 minetest.register_craftitem("default:copper_ingot", {
 	description = "Copper Ingot",
 	inventory_image = "default_copper_ingot.png",
-	groups = {ingot = 1},
+})
+
+minetest.register_craftitem("default:tin_ingot", {
+	description = "Tin Ingot",
+	inventory_image = "default_tin_ingot.png",
 })
 
 minetest.register_craftitem("default:bronze_ingot", {
 	description = "Bronze Ingot",
 	inventory_image = "default_bronze_ingot.png",
-	groups = {ingot = 1},
 })
 
 minetest.register_craftitem("default:gold_ingot", {
 	description = "Gold Ingot",
-	groups = {ingot = 1},
 	inventory_image = "default_gold_ingot.png"
 })
 
@@ -245,4 +332,3 @@ minetest.register_craftitem("default:flint", {
 	description = "Flint",
 	inventory_image = "default_flint.png"
 })
-
